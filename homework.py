e9432dc179a089548583db900340c36b7aa819a9
Alex Proskuriakov <alexpro1972@yandex.ru>
2022-06-08 11:01:29 +0300

    Final sprint7 rev.4

--- homework.py
+++ homework.py
@@ -29,59 +29,43 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
-SYSTEM_EXIT_PHRASE = 'Программа принудительно остановлена.'
-STATUS_NOTHING_PHRASE = 'Отсутствие в ответе новых статусов.'
-MISSING_TOKENS_PHRASE = (
+SYSTEM_EXIT = 'Программа принудительно остановлена.'
+STATUS_NOTHING = 'Отсутствие в ответе новых статусов.'
+MISSING_TOKENS = (
     'Отсутствует обязательная переменная окружения: {name}.')
-PARSE_STATUS_RETURN_PHRASE = (
+PARSE_STATUS_RETURN = (
     'Изменился статус проверки работы "{name}". {verdict}')
-PARSE_STATUS_ERROR_PHRASE = (
+PARSE_STATUS_ERROR = (
     'Неожиданный статус домашней работы в ответе API: {status}.')
-PRAKTIKUM_ATTRIBUTES = f'URL: {ENDPOINT}, Headers: {HEADERS} '
-PRAKTIKUM_INVALID_TOKEN = (
-    f'Проблема аутентификации, возможная причина - '
-    f'неверный токен Практикума. {SYSTEM_EXIT_PHRASE}')
-TELEGRAM_INFO = 'Telegram-Бот отправил сообщение: {message}.'
-TELEGRAM_BAD_REQUEST = (
-    'Telegram не может обработать запрос => '
-    'Возможные причины - неверный chat_id => '
-    'сбой при отправке Telegram-сообщения => {message}.')
-TELEGRAM_INVALID_TOKEN = (
-    f'Неверный Telegram токен => '
-    f'Невозможно активировать Бот => {SYSTEM_EXIT_PHRASE}')
-TELEGRAM_TIME_OUT = (
-    'Время запроса истекло => '
-    'сбой при отправке Telegram-сообщения => {message}.')
-TELEGRAM_UNAUTHORIZED = (
-    'У Бота не достаточно прав чтобы выполнить требуемое действие => '
-    'сбой при отправке Telegram-сообщения => {message}.')
-TELEGRAM_NETWORK_ERROR = (
-    'Проблемы с сетью => '
-    'сбой при отправке Telegram-сообщения => {message}.')
+TELEGRAM_BOT_ACTIVATION_FAILED = (
+    'Невозможно активировать Бот. Ошибка => {error}. '
+    'Передача сообщений в Telegram отключена.')
+TELEGRAM_INFO = 'Telegram-Бот отправил сообщение:\n"{message}"'
 TELEGRAM_ERROR = (
-    'Проблемы с Telegram => '
-    'сбой при отправке Telegram-сообщения => {message}.')
-HTTP_ERROR_PHRASE = 'Http статус: {status_code}.'
-REQUEST_ERROR_PHRASE = f'Ошибка запроса к серверу: {PRAKTIKUM_ATTRIBUTES}.'
-RESPONSE_IS_NOT_DICT_PHRASE = (
-    'Ответ API "JSON" не удалось преобразован в словарь.')
-KEY_IS_NOT_IN_RESPONSE_PHRASE = 'Отсутствие ключа ["homeworks"] в ответе API.'
-HOMEWORKS_IS_NOT_LIST_PHRASE = (
-    'Ответ API не содержит список под ключом ["homeworks"].')
-SERVER_ERROR_PHRASE = 'Сбой Сервера с ошибками: {server_errors}'
-MAIN_ERROR_PHRASE = 'Сбой в программе: {error}'
-
-
-class TokenError(Exception):
-    """Исключение для невалидных токенов."""
-
-    pass
+    'Сбой отправки Telegram-сообщения:\n"{message}"\n'
+    'Ошибка => {error}\n'
+    'Чат № {chat_id}')
+HTTP_ERROR = (
+    'Http статус: {status_code}.\nПараметры запроса => '
+    'URL: {url}, Заголовки: {headers}, Время: {params} сек.')
+REQUEST_ERROR = (
+    'Ошибка запроса к серверу.\nПараметры запроса => '
+    'URL: {url}, Заголовки: {headers}, Время: {params} сек.')
+SERVER_ERROR = (
+    'Сбой Сервера с ошибками: {server_errors}.\nПараметры запроса => '
+    'URL: {url}, Заголовки: {headers}, Время: {params} сек.')
+KEY_IS_NOT_IN_RESPONSE = 'Отсутствие ключа ["homeworks"] в ответе API.'
+RESPONSE_IS_NOT_DICT = (
+    'Ответ API "JSON" не удалось преобразован в словарь.\n'
+    'Тип ответа: {tipe}')
+HOMEWORKS_IS_NOT_LIST = (
+    'Ответ API не содержит список под ключом ["homeworks"].\n'
+    'Тип ответа: {tipe}')
+MAIN_ERROR = 'Сбой в программе. Ошибка => {error}'
 
 
 class ServerError(Exception):
     """Исключение для ошибок сервера Практикума."""
-
-    pass
 
 
 def check_tokens():
@@ -89,26 +73,25 @@ def check_tokens():
     ok = True
     for name in GLOBAL_VARIABLES_NAMES:
         if not globals()[name]:
-            logging.critical(MISSING_TOKENS_PHRASE.format(name=name))
-            if ok:
-                ok = False
+            logging.critical(MISSING_TOKENS.format(name=name))
+            ok = False
     return ok
 
 
 def get_api_answer(current_timestamp):
     """Делает запрос к единственному эндпоинту API-сервиса."""
+    params = {'from_date': current_timestamp}
     try:
-        response = requests.get(ENDPOINT, headers=HEADERS, params={
-            'from_date': current_timestamp})
+        response = requests.get(ENDPOINT, headers=HEADERS, params=params)
     except requests.exceptions.RequestException as err:
-        raise ServerError(
-            f'{current_timestamp} sec: {REQUEST_ERROR_PHRASE} {err}') from err
-    if response.status_code == 401:
-        raise TokenError(PRAKTIKUM_INVALID_TOKEN)
+        raise ConnectionError(
+            REQUEST_ERROR.format(
+                url=ENDPOINT, headers=HEADERS, params=params)) from err
     if response.status_code != 200:
         raise ServerError(
-            f'{current_timestamp} sec: ',
-            HTTP_ERROR_PHRASE.format(status_code=response.status_code))
+            HTTP_ERROR.format(
+                status_code=response.status_code,
+                url=ENDPOINT, headers=HEADERS, params=params))
     response_json = response.json()
     server_errors = []
     for item in ('code', 'error'):
@@ -116,7 +99,9 @@ def get_api_answer(current_timestamp):
             server_errors.append(f'{item} : {response_json[item]}')
     if server_errors:
         raise ServerError(
-            SERVER_ERROR_PHRASE.format(server_errors=server_errors))
+            SERVER_ERROR.format(
+                server_errors=server_errors,
+                url=ENDPOINT, headers=HEADERS, params=params))
     return response_json
 
 
@@ -128,12 +113,12 @@ def check_response(response):
     доступный в ответе API по ключу 'homeworks'.
     """
     if not isinstance(response, dict):
-        raise TypeError(RESPONSE_IS_NOT_DICT_PHRASE)
+        raise TypeError(RESPONSE_IS_NOT_DICT.format(tipe=type(response)))
     if 'homeworks' not in response:
-        raise KeyError(KEY_IS_NOT_IN_RESPONSE_PHRASE)
+        raise KeyError(KEY_IS_NOT_IN_RESPONSE)
     homeworks = response['homeworks']
     if not isinstance(homeworks, list):
-        raise TypeError(HOMEWORKS_IS_NOT_LIST_PHRASE)
+        raise TypeError(HOMEWORKS_IS_NOT_LIST.format(tipe=type(homeworks)))
     return homeworks
 
 
@@ -143,64 +128,65 @@ def parse_status(homework):
     status = homework['status']
     try:
         verdict = HOMEWORK_VERDICTS[status]
-    except KeyError as error:
-        raise KeyError(
-            PARSE_STATUS_ERROR_PHRASE.format(status=status)
-        ) from error
-    return PARSE_STATUS_RETURN_PHRASE.format(name=name, verdict=verdict)
+    except KeyError:
+        raise ValueError(PARSE_STATUS_ERROR.format(status=status))
+    return PARSE_STATUS_RETURN.format(name=name, verdict=verdict)
+
+
+def get_bot():
+    """Активирует бота."""
+    try:
+        return telegram.Bot(token=TELEGRAM_TOKEN)
+    except telegram.error.TelegramError as error:
+        logging.exception(TELEGRAM_BOT_ACTIVATION_FAILED.format(error=error))
 
 
 def send_message(bot, message):
     """Отправляет сообщение в Telegram чат."""
+    if not bot:
+        logging.info(message)
+        return True
     try:
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
         logging.info(TELEGRAM_INFO.format(message=message))
         return True
-    except telegram.error.BadRequest:
-        logging.exception(TELEGRAM_BAD_REQUEST.format(message=message))
-    except telegram.error.TimedOut:
-        logging.exception(TELEGRAM_TIME_OUT.format(message=message))
-    except telegram.error.Unauthorized:
-        logging.exception(TELEGRAM_UNAUTHORIZED.format(message=message))
-    except telegram.error.NetworkError:
-        logging.exception(TELEGRAM_NETWORK_ERROR.format(message=message))
-    except telegram.error.TelegramError:
-        logging.exception(TELEGRAM_ERROR.format(message=message))
-    return False
+    except telegram.error.TelegramError as error:
+        logging.exception(TELEGRAM_ERROR.format(
+            chat_id=TELEGRAM_CHAT_ID, message=message, error=error))
+        return False
 
 
-def main(bot):
+def main():
     """
     Основная логика работы бота.
     Сделать запрос к API. Проверить ответ. Если есть обновления —
     получить статус работы из обновления и отправить сообщение в Telegram.
     Подождать некоторое время и сделать новый запрос.
     """
+    if not check_tokens():
+        logging.critical(SYSTEM_EXIT)
+        return
+    bot = get_bot()
     current_timestamp = int(time.time())
-    prev_error, prev_status = '', ''
+    prev_error = ''
+    new_status = True
     while True:
         try:
             response_json = get_api_answer(current_timestamp)
             homeworks = check_response(response_json)
             if homeworks:
-                status = parse_status(homeworks[0])
-            else:
-                status = STATUS_NOTHING_PHRASE
-            if status != prev_status:
-                if status == STATUS_NOTHING_PHRASE:
-                    logging.info(status)
-                    prev_status = status
-                elif send_message(bot, status):
-                    prev_status = status
+                if send_message(bot, parse_status(homeworks[0])):
                     current_timestamp = response_json.get(
                         'current_date', current_timestamp)
+                    new_status = True
+            elif new_status:
+                logging.info(STATUS_NOTHING)
+                new_status = False
         except Exception as exc_error:
-            error = MAIN_ERROR_PHRASE.format(error=exc_error)
+            error = MAIN_ERROR.format(error=exc_error)
             logging.exception(error)
             if prev_error != error and send_message(bot, error):
                 prev_error = error
-            if isinstance(exc_error, TokenError):
-                return
         time.sleep(RETRY_TIME)
 
 
@@ -208,23 +194,15 @@ if __name__ == '__main__':
     logging.basicConfig(
         level=logging.INFO,
         format=(
-            '%(asctime)s, %(levelname)s, %(name)s, '
-            '%(funcName)s, %(lineno)s, %(message)s'
-        ),
+            '========================\n'
+            '%(asctime)s\n'
+            '%(levelname)s, %(name)s, %(funcName)s, %(lineno)s\n'
+            '%(message)s\n'
+            '========================\n'),
         handlers=[
+            logging.StreamHandler(sys.stdout),
             logging.FileHandler(
-                __file__.split('.')[0] + '.log',
-                encoding='utf-8'
-            ),
-            logging.StreamHandler(sys.stdout)
-        ]
-    )
-    if check_tokens():
-        try:
-            bot = telegram.Bot(token=TELEGRAM_TOKEN)
-        except telegram.error.InvalidToken:
-            logging.exception(TELEGRAM_INVALID_TOKEN)
-            raise TokenError(TELEGRAM_INVALID_TOKEN)
-        main(bot)
-    else:
-        logging.critical(SYSTEM_EXIT_PHRASE)
+                __file__[::-1].split('.')[1][::-1] + '.log',
+                encoding='utf-8')
+        ])
+    main()
